3ea44448f149f9b4bc51ca929d542d088cf8d5db
Quentin Altieri <72386166+quentalt@users.noreply.github.com>
2024-06-14 18:39:39 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,20 +5,23 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import WinnerPage from './WinnerPage';
-import Generic from './Generic';
+import LoserPage from './LoserPage';
+import Introduction from './Introduction';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Generic />} />
+            <Route path="/" element={<Introduction />} />
             <Route path="/App" element={<App />} />
             <Route path="/winner" element={<WinnerPage />} />
+            <Route path="/loser" element={<LoserPage/>} />
+
         </Routes>
     </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
+// to log results (for example: reportWebVitals.console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
